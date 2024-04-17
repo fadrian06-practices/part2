d059a87fc32458955817d8c7b8dd975f94b6e659
fadrian06 <franyeradriansanchez@gmail.com>
2024-04-17 15:33:00 -0400

    Adding show importants filter

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,14 @@ import Note from './components/Note'
 const App = props => {
   const [notes, setNotes] = useState(props.notes)
   const [newNote, setNewNote] = useState('A new note...')
+  const [showAll, setShowAll] = useState(true)
 
   const addNote = event => {
     event.preventDefault()
 
     const note = {
       id: notes.length + 1,
-      important: Math.random() < .5,
+      important: Math.random() < 0.5,
       content: newNote
     }
 
@@ -20,11 +21,18 @@ const App = props => {
 
   const handleNoteChange = event => setNewNote(event.target.value)
 
+  const notesToShow = showAll ? notes : notes.filter(note => note.important === true)
+
   return (
     <div>
       <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          Show {showAll ? 'important' : 'all'}
+        </button>
+      </div>
       <ul>
-        {notes.map(note => (
+        {notesToShow.map(note => (
           <Note key={note.id} note={note} />
         ))}
       </ul>
