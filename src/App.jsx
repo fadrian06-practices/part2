c5aba88ba35bdf2c7e3c63cf6ba37e7ceca41793
fadrian06 <franyeradriansanchez@gmail.com>
2024-04-17 15:29:05 -0400

    Adding notes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,25 @@
+import { useState } from 'react'
 import Note from './components/Note'
 
-const App = ({ notes }) => {
+const App = props => {
+  const [notes, setNotes] = useState(props.notes)
+  const [newNote, setNewNote] = useState('A new note...')
+
+  const addNote = event => {
+    event.preventDefault()
+
+    const note = {
+      id: notes.length + 1,
+      important: Math.random() < .5,
+      content: newNote
+    }
+
+    setNotes(notes.concat(note))
+    setNewNote('')
+  }
+
+  const handleNoteChange = event => setNewNote(event.target.value)
+
   return (
     <div>
       <h1>Notes</h1>
@@ -9,6 +28,10 @@ const App = ({ notes }) => {
           <Note key={note.id} note={note} />
         ))}
       </ul>
+      <form onSubmit={addNote}>
+        <input onChange={handleNoteChange} value={newNote} />
+        <button>Save</button>
+      </form>
     </div>
   )
 }
